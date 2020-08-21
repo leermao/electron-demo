51d06e44c1edefbfdb2e86e6c2fffe0e82da9ac2
lipeng1 <447552708@qq.com>
2020-08-21 11:23:41 +0800

    feat:init

---
+++ app/main/index.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron");
+const isDev = require("electron-is-dev");
+const path = require("path");
+
+let win = null;
+app.on("ready", () => {
+  win = new BrowserWindow({
+    width: 600,
+    height: 300,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  if (isDev) {
+    win.loadURL("http://localhost:3000");
+  } else {
+    win.loadFile(path.resolve(__dirname, "../renderer/pages/main/index.html"));
+  }
+});
